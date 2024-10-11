27dc0e2ea72181b2414eb9d3ff509e2033a0af4c
Skyzimos <95323257+Skyzimos@users.noreply.github.com>
2024-10-11 09:51:39 -0500

    Update script.js

--- practice/games/chapter/3/quiz/script.js
+++ practice/games/chapter/3/quiz/script.js
@@ -280,10 +280,15 @@ const uid = () => {
 localStorage.setItem('last-played', chapterNumber);
 
 function shuffleArray(array) {
+  let new_arr = [];
+  
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1)); // Random index from 0 to i
-    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    new_arr[j] = array[i]
+    // [array[i], array[j]] = [array[j], array[i]]; // Swap elements
   }
+
+  console.log(array)
   return array;
 }
 
