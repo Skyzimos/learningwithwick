7a7dd7e1b6ce4e6cad67656992002029afd9e4f6
Skyzimos <95323257+Skyzimos@users.noreply.github.com>
2024-10-14 18:07:31 -0500

    Update loading.js

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,1 +1,42 @@
+for (let child of document.getElementById('sidebar-buttons-container').children) {
+  if (child.className === 'sidebar-button') {
+    child.onclick = function() {
+      document.getElementById('loading-overlay').classList.remove('fade-out')
+      document.getElementById('loading-overlay').style.opacity = 0;
+      document.getElementById('loading-overlay').style.display = 'block';
+      document.getElementById('loading-overlay').classList.add('fade-in')
 
+      setTimeout(() => {
+        location.href = child.id;
+      }, 250);
+    }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(() => {
+    document.getElementById('loading-overlay').classList.add('fade-out');
+
+    setTimeout(() => {
+      document.getElementById('loading-overlay').style.display = 'none';
+    }, 250)
+  }, 1000)
+});
+
+if (sessionStorage.getItem('loaded') === null) {
+  document.getElementById('loader-overlay').style.display = 'flex';
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(() => {
+      document.body.classList.add('loaded');
+
+      setTimeout(() => {
+        document.getElementById('loader-overlay').style.display = 'none';
+      }, 500);
+    }, 2500)
+  });
+
+  sessionStorage.setItem('loaded', true)
+} else {
+  document.getElementById('loader-overlay').style.display = 'none';
+}
