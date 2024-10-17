ea68ed4ed8bc36f2e47b9d4ffc483aa9c2266a5f
Skyzimos <jessemartin0710@gmail.com>
2024-10-16 21:54:16 -0500

    Updated to latest framework

--- practice/games/chapter/3/quiz/script.js
+++ practice/games/chapter/3/quiz/script.js
@@ -747,6 +747,7 @@ function gradeQuiz(quizData) {
   Object.keys(quizData).forEach((questionKey, index) => {
     const questionObj = quizData[questionKey];
     const userAnswer = selectedAnswers[index]; // Get user answer for the question
+    console.log(userAnswer);
 
     if (questionObj.type === 'multiple_choice') {
       const correctAnswersArray = questionObj.data.answers; // Get the correct answers
